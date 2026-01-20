3a7c0d8fc740698f4b0780c425e9afb645c5e38e
ayselmahmudova04 <aysel2004mahmudova@gmail.com>
2026-01-20 16:47:51 +0400

    icons

--- realEstate/src/App.jsx
+++ realEstate/src/App.jsx
@@ -1,12 +1,10 @@
 import React, { useEffect } from 'react'
 
 function App() {
-  
+
   return (
     <>
-      <div className="bg-blue-500 text-white p-4">
-        Test
-      </div>
+      
     </>
   )
 }
